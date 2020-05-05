593abd2fd446d1adab0896efa1120490b01daced
Manijas <zerocool.moreno@gmail.com>
2020-05-05 10:52:02 -0500

    FIX:Se cambia el llamado al procedimiento

--- app/Http/Controllers/ReportesController.php
+++ app/Http/Controllers/ReportesController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Maatwebsite\Excel\Facades\Excel;
 use App\Exports\ContratistasExport;
 use Illuminate\Http\Request;
@@ -8,40 +9,37 @@ use DB;
 
 class ReportesController extends Controller
 {
-     public function __construct()
+    public function __construct()
     {
         $this->middleware('auth');
     }
 
-     public function index(Request $request)
+    public function index(Request $request)
     {
-             if ($request) {
-			
-			return view('Reportes.HorasHombre.index');
-    	}
+        if ($request) {
+
+            return view('Reportes.HorasHombre.index');
+        }
     }
 
     public function store(Request $request)
     {
-        $mes=$request->post('mes');
-        
-        
-        $Tipo=$request->post('tipo');
-       // dd($Tipo);
-        
-        $data=DB::select('call sp_reporte_horashombre(?,?)' ,array($mes,$Tipo));
-        
-        $resultados=DB::table('reportehh')->count();
+        $mes = $request->post('mes');
+        $Tipo = $request->post('tipo');
+
+
+        $data=DB::statement('call sp_reporte_horashombre(?,?)' ,array($mes,$Tipo));
+        //$data = DB::select('exec sp_reporte_horashombre(?,?)',array($mes,$Tipo));
+
+        $resultados = DB::table('reportehh')->count();
         //dd($resultados);
-        if ($resultados>1) {
+        if ($resultados > 1) {
             return Excel::download(new ContratistasExport, 'HorasHombre.xlsx');
-        }
-        else
-        {
+        } else {
             return view("Reportes.HorasHombre.mensaje", ["errores" => "La consulta no arrojo ningún resultado"]);
 
         }
-        
+
     }
 
     public function principal(Request $request)
