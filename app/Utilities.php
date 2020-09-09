e4ff8cc3f6d6727a633bc91dd8df6f970271f5b1
Manuel Moreno <manuel.moreno@televisatim.com>
2020-09-09 13:25:08 -0500

    ADD:Se agrega metodo para comparar fechas

--- app/Utilities.php
+++ app/Utilities.php
@@ -10,11 +10,48 @@ class Utilities extends Model
     {
         $data = base64_decode($variables);
         $parametros = explode('&', $data);
-        foreach($parametros as $llave => $parametro){
-           $separaVariables = explode('=', $parametro);
-           $nuevoArray[$separaVariables[0]] =  $separaVariables[1];
+        foreach ($parametros as $llave => $parametro) {
+            $separaVariables = explode('=', $parametro);
+            $nuevoArray[$separaVariables[0]] = $separaVariables[1];
         }
         return $nuevoArray;
+    }
+
+
+    /**
+     * @param        $date_1
+     * @param        $date_2
+     * @param string $differenceFormat
+     *
+     * PARA: Date Should In YYYY-MM-DD Format
+     * RESULT FORMAT:
+     * '%y Year %m Month %d Day %h Hours %i Minute %s Seconds'  =>  1 Year 3 Month 14 Day 11 Hours 49 Minute 36 Seconds
+     * '%y Year %m Month %d Day' =>  1 Year 3 Month 14 Days
+     *  '%m Month %d Day'                                            =>  3 Month 14 Day
+     *                                '%d Day %h Hours'                                            =>  14 Day 11 Hours
+     *                                '%d Day'                                                        =>  14 Days
+     *                                %h Hours %i Minute %s Seconds'                                =>  11 Hours 49
+     *                                Minute 36 SECONDS
+     *                                '%i Minute %s Seconds'                                        =>  49 Minute 36
+     *                                Seconds
+     *                                '%h Hours                                                    =>  11 Hours
+     *                                '%a Days                                                        =>  468 Days
+     *
+     *
+     *
+     *
+     * @return string
+     */
+    public function dateDifference($date_1, $date_2, $differenceFormat = '%a')
+    {
+        //$timeZone = 'America/Mexico city';
+        $datetime1 = date_create($date_1);
+        $datetime2 = date_create($date_2);
+
+        $interval = date_diff($datetime1, $datetime2);
+
+        return $interval->format($differenceFormat);
+
     }
 
 }
